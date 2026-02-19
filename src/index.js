68d434908bcd6af0d1af45fbf58cf4b44dffd3c9
HassanTantawy-dev <hasantantawy@icloud.com>
2026-02-19 17:26:47 +0200

    Add signup route to the router, integrating AllSignup component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import {createBrowserRouter,RouterProvider,} from "react-router";
 import App from "./App";
 import { AllData } from "./Data/AllData";
 import { createRoot } from "react-dom/client";
+import AllSignup from "./Signup/AllSignup";
 // import ScrollToTop from "react-scroll-to-top";
 
 let router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "signup",
+    element: <AllSignup />,
   },
 ]);
 
