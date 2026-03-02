a326274b2896ea0e5786417436715ff1eee74557
Hassan Tantawy <hasantantawy@icloud.com>
2026-03-02 14:07:47 +0200

    push

--- src/Deatials/Alldeatial.js
+++ src/Deatials/Alldeatial.js
@@ -1,31 +1,26 @@
-import React from 'react' 
-import Footer from '../components/footer'
-import Comments from '../Home/comments'
-import Courses from './Courses'
-import Navbar2 from '../components/Navbar2'
-import { useState } from 'react'
-<<<<<<< HEAD
-import Header1 from './Header1'
-=======
-import Content from './Content'
->>>>>>> e24b63236ce2336c2a9f20eb272f90a3d7ffd92e
+import React from "react";
+import Footer from "../components/footer";
+import Comments from "../Home/comments";
+import Courses from "./Courses";
+import Navbar2 from "../components/Navbar2";
+import { useState } from "react";
+import Header1 from "./Header1";
+import Content from "./Content";
 
 function Alldeatial() {
   const [search, setSearch] = useState("");
   return (
-      <div>
-<<<<<<< HEAD
-          <Navbar2 search={search} setSearch={setSearch} />
-          <Header1 />
-=======
+    <div>
+      <Navbar2 search={search} setSearch={setSearch} />
+      <Header1 />
       <Navbar2 search={search} setSearch={setSearch} />
       <Content />
->>>>>>> e24b63236ce2336c2a9f20eb272f90a3d7ffd92e
-          <Comments />
-          <Courses/>
-          <Footer/>
+
+      <Comments />
+      <Courses />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Alldeatial
+export default Alldeatial;
